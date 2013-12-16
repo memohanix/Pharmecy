86f0b9a3a73874af80d47bdc9a49264c2c2d98b2
Mohanix <im.mohanix@gmail.com>
2013-12-16 15:41:24 +0545

    First time push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pharmacy::Application.config.secret_token = '268c98bba44f3c398c153e2fcb3cf9719c4375a4cd59f29ab372d2866f652d7be47fcdf119c22d083b565e23f1533b4d60684bf15cb6da36157b9eb961bc0338'
